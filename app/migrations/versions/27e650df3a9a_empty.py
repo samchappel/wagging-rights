74912def1a21a5c6169beaf16d90149bba736b3d
Terrence Chung <terrencechung35@gmail.com>
2023-03-07 00:28:02 -0800

    terrenceMondaze

---
+++ app/migrations/versions/27e650df3a9a_empty.py
@@ -0,0 +1,24 @@
+"""empty
+
+Revision ID: 27e650df3a9a
+Revises: 
+Create Date: 2023-03-07 00:27:17.897239
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '27e650df3a9a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
