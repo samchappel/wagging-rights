b1dc1d0119777a1daaf8aedca2df9a64145f37ad
Terrence Chung <terrencechung35@gmail.com>
2023-03-07 21:10:50 -0800

    updated availability to 3 days instead of 1 in services table, and included corresponding start and  end dates for each available day

--- app/seeds.py
+++ app/seeds.py
@@ -73,11 +73,17 @@ if __name__ == '__main__':
         providers = []
 
         for _ in range(10):
+            start_date = fake.date_time_between(start_date="now", end_date="+3d", tzinfo=None)
+            end_date = start_date + timedelta(days=random.randint(1, 3))
             provider = Provider(
                 name = f"{fake.first_name()} {fake.last_name()}",
                 email = fake.email(),
                 phone = random.randint(1000000000, 9999999999),
-                availability = fake.day_of_week()
+                availability = f"""
+{fake.day_of_week()} ({start_date} - {end_date }), \n
+{fake.day_of_week()} ({start_date} - {end_date }), \n
+{fake.day_of_week()} ({start_date} - {end_date }) \n
+""",
                 # hourly_rate = f'$' + f'%.2f' % random.uniform(20,40),
             )
 
@@ -94,16 +100,17 @@ if __name__ == '__main__':
         for provider in providers:
 
             for _ in range(random.randint(1,10)):
+                date = fake.date_time_between(start_date="now", end_date="+1y", tzinfo=None)
                 service = Service(
                     request = random.choice(requests),
-                    start_date = fake.date_time_between(start_date="-1y", end_date="now", tzinfo=None),
-                    end_date = fake.date_time_between(start_date="-1y", end_date="now", tzinfo=None) + timedelta(days=random.randint(1, 7)),
+                    start_date = date,
+                    end_date = date + timedelta(days=random.randint(1, 3)),
                     notes=fake.sentence(),
                     fee = f'$' + f'%.2f' % random.uniform(20,40),
                     provider_id = provider.id,
                     pet_id = random.choice(pets).id
                 )
-            services.append(service)
+                services.append(service)
 
     session.bulk_save_objects(services)
     session.commit()
