d7d35a4778f5c74891f388819ced41ec29e9adea
Bianca Aspin <bianca@mj94.net>
2023-03-07 16:43:14 -0800

    Add comments with initial flow for navigating to Main Menu for Pets

--- app/cli.py
+++ app/cli.py
@@ -3,9 +3,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Store
-from helpers import (create_store_table, create_wagging_rights_item_table, fill_cart, show_cart, remove_from_cart, collect_payment)
-
 engine = create_engine('sqlite:///wagging_rights.db')
 session = sessionmaker(bind=engine)()
 
@@ -28,36 +25,6 @@ __          __     _____  _____ _____ _   _  _____   _____  _____ _____ _    _ _
                                     ____|_       ___|   |___.'
                                     /_/_____/____/_______|
 
-''')
+    ''')
+    print("Welcome to the Wagging Rights CLI!")
 
-
-
-
-    # print('Hello! Welcome to the Wagging Rights CLI!')
-    # print('Here is a list of available services:')
-    # stores = session.query(Store)
-    # create_store_table(stores)
-
-    # # Get a choice of store, retrieve an object from the DB
-    # store= None
-    # while not store:
-    #     store_id = input('Please enter the ID of the service that you wish to select: ')
-    #     store = session.query(Store).filter(Store.id == store_id).one_or_none()
-
-    # # Display list of items at the store
-    # print('Here is a list of our services:')
-    # create_service_item_table(store)
-
-    # # Start adding items to cart
-    # shopping_cart, cart_total = fill_cart(session, store)
-    # print('Here are the services in your cart:')
-    # show_cart(shopping_cart)
-
-    # # Remove unwatned items from cart
-    # remove_from_cart(session, shopping_cart, cart_total)
-
-    # #Collect payment
-    # print(f'Your total is ${cart_total:.2f}\n')
-    # collect_payment(cart_total)
-
-    # print('Thank you for using the Wagging Rights CLI!\n ')
