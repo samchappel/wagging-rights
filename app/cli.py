b39775dd77cbe10efdee029f58302ea39d1ae271
Bianca Aspin <bianca@mj94.net>
2023-03-07 20:41:51 -0800

    Refactor 'add pet' functionality to 'add_new_pet' helper function.

--- app/cli.py
+++ app/cli.py
@@ -7,6 +7,7 @@ engine = create_engine('sqlite:///wagging_rights.db')
 session = sessionmaker(bind=engine)()
 
 from models import Owner, Pet, Provider, Service
+from helpers import add_new_pet
 
 
 if __name__ == '__main__':
@@ -64,13 +65,14 @@ __          __     _____  _____ _____ _   _  _____   _____  _____ _____ _    _ _
                 temperament = input("Temperament: ")
                 treats = input("Favorite Treats: ")
                 notes = input("Additional Notes/Special Needs: ")
-                new_pet = Pet(name=name, age=age, breed=breed, 
-                              temperament=temperament, favorite_treats=treats, notes=notes, owner_id=owner_id)
-                session.add(new_pet)
-                session.commit()
-                new_db_pet = session.query(Pet).filter(Pet.id == new_pet.id).first()
-                print("Thank you for your submission. Here is the information we received:")
-                print(new_db_pet)
+                # new_pet = Pet(name=name, age=age, breed=breed, 
+                #               temperament=temperament, favorite_treats=treats, notes=notes, owner_id=owner_id)
+                # session.add(new_pet)
+                # session.commit()
+                # new_db_pet = session.query(Pet).filter(Pet.id == new_pet.id).first()
+                # print("Thank you for your submission. Here is the information we received:")
+                # print(new_db_pet)
+                add_new_pet(session, name, age, breed, temperament, treats, notes, owner_id)
                 another = input("Would you like to add another pet? Yes/No: ").lower()
                 if another == "no":
                     print("Routing you back to the main menu...")
--- app/helpers.py
+++ app/helpers.py
@@ -1,43 +1,10 @@
-# from models import Service
+from models import Pet
 
-# YES = ['y', 'ye', 'yes']
-# NO = ['n', 'no']
-
-# def create_request_table(requests):
-#     print('-' * 50)
-#     print(f'|ID |NAME{" " * 39}|')
-#     print('-' * 50)
-#     for request in requests:
-#         id_spaces = 4 - len(str(request.id))
-#         name_spaces = 43 - len(request.name)
-#         print(f'|{request.id}{" " * id_spaces}|{request.name}{" " * name_spaces}|')
-#     print('-' * 50)
-
-# def create_item_table(request):
-#     print('-' * 50)
-#     print(f'|ID  |ITEM NAME{" " * 24}|PRICE{" " * 4}|')
-#     print('-' * 50)
-#     for item in sorted(request.items, key=lambda g: g.id):
-#         id_spaces = 4 - len(str(item.id))
-#         name_spaces = 33 - len(item.name)
-#         price_spaces = 8 - len(f'{item.price:.2f}')
-#         output_string = f'|{item.id}{" " * id_spaces}|' + \
-#             f'{item.name}{" " * name_spaces}|' + \
-#             f'${item.price:.2f}{" " * price_spaces}|'
-#         print(output_string)
-#     print('-' * 50)
-
-# def fill_cart(session, request):
-#     shopping_cart = ShoppingCart(request=request)
-#     item_id = input('Please enter the ID of your first item: ')
-#     cart_total = 0
-#     while item_id:
-#         item = session.query(Service).filter(
-#             Service.id==item_id).first()
-#         if item in request.items:
-#             shopping_cart.items.append(item)
-#             cart_total += item.price
-#             print(f'Cart total is now ${cart_total:.2f}\n')
-#         else:
-#             item_id = input('Please enter a valid service item ID: ')
-#             continue
+def add_new_pet(session, name, age, breed, temperament, treats, notes, owner_id):
+    new_pet = Pet(name=name, age=age, breed=breed, temperament=temperament, 
+                favorite_treats=treats, notes=notes, owner_id=owner_id)
+    session.add(new_pet)
+    session.commit()
+    new_db_pet = session.query(Pet).filter(Pet.id == new_pet.id).first()
+    print("Thank you for your submission. Here is the information we received:")
+    print(new_db_pet)
